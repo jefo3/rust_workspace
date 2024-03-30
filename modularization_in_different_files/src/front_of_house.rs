cab77063e874ea182f9d1f41273ba0a20fd2fda4
Jeferson Gonçalves <jeferson.goncalves.noronha@gmail.com>
2024-03-30 01:31:32 -0300

    feat: Separating Modules into Different Files

---
+++ modularization_in_different_files/src/front_of_house.rs
@@ -0,0 +1,5 @@
+pub mod hosting {
+    pub fn add_to_waitlist() {
+        print!("add in waitlist successful!")
+    }
+}
---
+++ modularization_in_different_files/src/main.rs
@@ -0,0 +1,5 @@
+use modularization_in_different_files::eat_at_restaurant;
+
+fn main() {
+    eat_at_restaurant();
+}
